e34ea8e0d5cbc85a9a0a789791083acb7faf8687
koonukaame <marialezhebokova@yandex.ru>
2025-09-03 01:00:56 +0300

    chore: setup eslint, prettier & husky

--- eslint-strict.config.mjs
+++ eslint-strict.config.mjs
@@ -1,12 +1,20 @@
 import { FlatCompat } from '@eslint/eslintrc';
+import js from '@eslint/js';
 
 const compat = new FlatCompat({
   baseDirectory: import.meta.dirname,
+  recommendedConfig: js.configs.recommended,
 });
 
 const eslintConfig = [
   ...compat.config({
-    extends: ['next/core-web-vitals'],
+    extends: [
+      'eslint:recommended',
+      'next/core-web-vitals',
+      'next/typescript',
+      'plugin:prettier/recommended',
+    ],
+    plugins: ['prettier'],
   }),
   {
     ignores: [
