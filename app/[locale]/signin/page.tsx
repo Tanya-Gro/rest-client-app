16a4a8d17ddc8ff82bc7d863c2daf2d45b87647e
Viktor1905 <webdevvva@gmail.com>
2025-09-06 14:23:15 +0300

    feat: delete h1 on signup and signin pages

--- app/[locale]/signin/page.tsx
+++ app/[locale]/signin/page.tsx
@@ -3,7 +3,6 @@ import { SignInForm } from './components/signInForm';
 export default function SignIn() {
   return (
     <>
-      <h1>Sign in</h1>
       <SignInForm />
     </>
   );
--- app/[locale]/signup/page.tsx
+++ app/[locale]/signup/page.tsx
@@ -3,7 +3,6 @@ import { SignUpForm } from './components/signUpForm';
 export default function SignUp() {
   return (
     <>
-      <h1>Sign up</h1>
       <SignUpForm />
     </>
   );
