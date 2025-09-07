f687fcd9b3018acc8a444ecadc44781601970473
unknown <tarpel.gta@gmail.com>
2025-09-07 15:37:38 +0300

    style: remove not useful classes and messages

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,6 +1,3 @@
-import { useTranslations } from 'next-intl';
-
 export default function Home() {
-  const t = useTranslations('main');
-  return <h1 className="max-w-[1200px] mx-auto">{t('text')}</h1>;
+  return <h1>Main Page</h1>;
 }
