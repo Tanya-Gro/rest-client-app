df3b2b8b45a8277fc4509d1dcc4eea4ba7cb6039
koonukaame <marialezhebokova@yandex.ru>
2025-09-08 16:33:17 +0300

    feat: localize schema messages

--- src/entities/client.ts
+++ src/entities/client.ts
@@ -1,48 +1,47 @@
 import { z } from 'zod';
 
-export const Client = z
-  .object({
-    method: z.enum([
-      'get',
-      'post',
-      'put',
-      'patch',
-      'delete',
-      'head',
-      'options',
-    ]),
-    url: z.url('Enter correct URL'),
-    bodyType: z.enum(['json', 'text']),
-    body: z.string().optional(),
-    headers: z
-      .array(
-        z.object({
-          header: z.string().optional(),
-          value: z.string().optional(),
-        })
-      )
-      .optional(),
-  })
-  .refine(
-    (fields) => {
-      if (!fields.body) {
-        return true;
-      }
-
-      if (fields.bodyType === 'json') {
-        try {
-          JSON.parse(fields.body);
+export const Client = (t: (arg: string) => string) =>
+  z
+    .object({
+      method: z.enum([
+        'get',
+        'post',
+        'put',
+        'patch',
+        'delete',
+        'head',
+        'options',
+      ]),
+      url: z.url(t('urlError')),
+      bodyType: z.enum(['json', 'text']),
+      body: z.string().optional(),
+      headers: z
+        .array(
+          z.object({
+            header: z.string().optional(),
+            value: z.string().optional(),
+          })
+        )
+        .optional(),
+    })
+    .refine(
+      (fields) => {
+        if (!fields.body) {
           return true;
-        } catch {
-          return false;
         }
-      }
-      return true;
-    },
-    {
-      error: 'Incorrect JSON format',
-      path: ['body'],
-    }
-  );
 
-export type Client = z.infer<typeof Client>;
+        if (fields.bodyType === 'json') {
+          try {
+            JSON.parse(fields.body);
+            return true;
+          } catch {
+            return false;
+          }
+        }
+        return true;
+      },
+      {
+        error: t('jsonError'),
+        path: ['body'],
+      }
+    );
