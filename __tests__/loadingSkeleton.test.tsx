200178c6bba23098322a824a63d8180ae594e12c
Viktor1905 <webdevvva@gmail.com>
2025-09-21 15:52:25 +0300

    feat: done loading test

---
+++ __tests__/loadingSkeleton.test.tsx
@@ -0,0 +1,13 @@
+import { render } from '@testing-library/react';
+import Loading from "../app/[locale]/loading";
+
+describe('Loading page', () => {
+  it('Loading rendered', async () => {
+
+    const { container } = render(<Loading />);
+    const skeletons = container.querySelectorAll('.h-\\[40px\\]');
+    expect(skeletons.length).toBeGreaterThan(3);
+
+  });
+});
+
