f1150ed80327f68ea8a6593881bfac12927f0fc5
Kiterina <webdevvva@gmail.com>
2025-09-21 17:07:33 +0300

    feat: done footer test

---
+++ __tests__/footer.test.tsx
@@ -0,0 +1,11 @@
+import { Footer } from '@components';
+import { render, screen } from '@testing-library/react';
+
+const link = 'https://rs.school/courses/reactjs'
+describe('Footer render test', () => {
+  it('render footer', async () => {
+    render(<Footer/>)
+
+    expect(screen.getByRole('link',{ name: /RS School/i  })).toHaveAttribute('href', link)
+  })
+})
