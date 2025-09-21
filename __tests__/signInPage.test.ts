19f06809a1d4005c4b6a61be70e329a6f13f45e1
Kiterina <webdevvva@gmail.com>
2025-09-21 19:25:39 +0300

    feat: signInPage + signUpPage test

---
+++ __tests__/signInPage.test.ts
@@ -0,0 +1,16 @@
+import { describe, it, expect, vi } from 'vitest';
+import { render } from '@testing-library/react';
+import SignIn from '../app/[locale]/(public)/signin/page';
+
+vi.mock('next-intl/server', () => ({
+  getTranslations: vi.fn().mockResolvedValue((key: string) => key),
+}));
+
+describe('SignUp component', () => {
+  it('renders header, description and form', async () => {
+    const { findByText } = render(await SignIn());
+
+    expect(await findByText('login.header')).toBeTruthy();
+    expect(await findByText('login.header-description')).toBeTruthy();
+  });
+});
---
+++ __tests__/signUpPage.test.ts
@@ -0,0 +1,16 @@
+import { describe, it, expect, vi } from 'vitest';
+import { render } from '@testing-library/react';
+import SignUp from '../app/[locale]/(public)/signup/page';
+
+vi.mock('next-intl/server', () => ({
+  getTranslations: vi.fn().mockResolvedValue((key: string) => key),
+}));
+
+describe('SignUp component', () => {
+  it('renders header, description and form', async () => {
+    const { findByText } = render(await SignUp());
+
+    expect(await findByText('registration.header')).toBeTruthy();
+    expect(await findByText('registration.header-description')).toBeTruthy();
+  });
+});
