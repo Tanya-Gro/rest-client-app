22454fbe25bd1d7508152e9bb81c4a055d6980ad
Kiterina <webdevvva@gmail.com>
2025-09-21 17:07:48 +0300

    feat: done header test

---
+++ __tests__/header.test.tsx
@@ -0,0 +1,22 @@
+import { Header } from '@components';
+import { render, screen } from '@testing-library/react';
+import { vi, describe, it, expect } from 'vitest';
+
+vi.mock('next-intl', () => ({
+  useTranslations: () => (key: string) => key,
+  useLocale: () => 'en',
+  Link: ({ href, children }: any) => <a href={href}>{children}</a>,
+}))
+
+vi.mock('@i18n', () => ({
+  useRouter: () => ({ push: vi.fn() }),
+  usePathname: () => '/',
+  Link: ({ href, children }: any) => <a href={href}>{children}</a>,
+}))
+describe('Header render test', () => {
+  it('render header', async () => {
+    render(<Header status='public'/>)
+
+    expect(screen.getByRole('heading',{ level: 1  })).toHaveTextContent('REST API Client')
+  })
+})
