ffad602840bc18975a10ca92e7b1620fb31bd110
Viktor1905 <webdevvva@gmail.com>
2025-09-21 15:52:47 +0300

    feat: Main test

---
+++ __tests__/mainPage.test.tsx
@@ -0,0 +1,27 @@
+import { vi } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import Main from "../app/[locale]/(private)/main/page";
+
+vi.mock('@components', () => ({
+  Button: ({ children }: any) => <button>{children}</button>,
+}));
+
+vi.mock('@i18n', () => ({
+  Link: ({ href, children }: any) => <a href={href}>{children}</a>,
+}));
+
+vi.mock('next-intl/server', () => ({
+  getTranslations: async () => (key: string) => key,
+}));
+
+describe('Main page', () => {
+  it('renders welcome text and buttons', async () => {
+    const Component = await Main();
+    render(Component);
+    const expectedHrefs = ['/rest-client', '/history', '/variables'];
+    const links = screen.getAllByRole('link');
+    links.forEach((link, index) => {
+      expect(link).toHaveAttribute('href', expectedHrefs[index]);
+    });
+  });
+});
